683262d44d7b1214a7abb44a87a67773b01323e2
Thomas Moffet <pantherman1996@gmail.com>
2017-05-08 11:38:11 +1000

    Created the window.

--- MapViewer/src/Main.cpp
+++ MapViewer/src/Main.cpp
@@ -1,6 +1,46 @@
 #include <Windows.h>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
-	MessageBox(NULL, L"This program has many features.", L"MapViewer", MB_ICONINFORMATION);
-	return 0;
+#include <windowsx.h>
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	switch (message) {
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return 0;
+	}
+
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+
+	// This whole window creation was copied from http://www.directxtutorial.com/Lesson.aspx?lessonid=9-1-3.
+
+	HWND hWnd;
+	WNDCLASSEX wc;
+
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.lpszClassName = L"WindowClass1";
+
+	RegisterClassEx(&wc);
+
+	hWnd = CreateWindowEx(NULL, L"WindowClass1", L"MapViewer", WS_OVERLAPPEDWINDOW, 300, 300, 500, 400, NULL, NULL, hInstance, NULL);
+
+	ShowWindow(hWnd, nCmdShow);
+
+	MSG msg;
+
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
 }
