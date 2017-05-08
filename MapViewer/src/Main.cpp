8f92f432cebd4ab38411b45b84dbf1d635077ed3
Thomas Moffet <pantherman1996@gmail.com>
2017-05-08 11:27:07 +1000

    Changed the project to a Windows application.

--- MapViewer/src/Main.cpp
+++ MapViewer/src/Main.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
+#include <Windows.h>
 
-int main(int argc, char* argv[]) {
-	std::cout << "This program has many features.\n";
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+	MessageBox(NULL, L"This program has many features.", L"MapViewer", MB_ICONINFORMATION);
 	return 0;
 }
